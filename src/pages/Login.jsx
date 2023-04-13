146eb2cb4de8c266bb2f04041b6a7408d32d557c
amanda-vlopes <amandavitor@hotmail.com>
2023-04-13 17:54:57 -0300

    Requisito 2 finalizado

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,66 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { createUser } from '../services/userAPI';
+import Carregando from './Carregando';
 
 export default class Login extends Component {
+  state = {
+    nameLogin: '',
+    buttonDisabled: true,
+    loading: false,
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    const tamanhoMinimo = 3;
+    this.setState({
+      [name]: value,
+      buttonDisabled: value.length < tamanhoMinimo,
+    });
+  };
+
+  handleLogin = async () => {
+    const { nameLogin } = this.state;
+    const { history } = this.props;
+    this.setState({ loading: true });
+    await createUser({ name: nameLogin });
+    history.push('/search');
+  };
+
   render() {
+    const { nameLogin, buttonDisabled, loading } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        {loading
+          ? (<Carregando />)
+          : (
+            <form action="">
+              <input
+                type="text"
+                name="nameLogin"
+                id="nameLogin"
+                data-testid="login-name-input"
+                placeholder="Digite o seu nome"
+                onChange={ this.onInputChange }
+                value={ nameLogin }
+              />
+              <button
+                data-testid="login-submit-button"
+                disabled={ buttonDisabled }
+                onClick={ this.handleLogin }
+              >
+                Entrar
+              </button>
+            </form>
+          )}
+
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
