b8981edea5a14a0ece24a3bab01dbe7f38b2f5e7
amanda-vlopes <amandavitor@hotmail.com>
2023-04-13 19:22:56 -0300

    Requisito 5 finalizado

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    artistInput: '',
+    buttonDisabled: true,
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    const tamanhoMinimo = 2;
+    this.setState({
+      [name]: value,
+      buttonDisabled: value.length < tamanhoMinimo,
+    });
+  };
+
   render() {
+    const { artistInput, buttonDisabled } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search">Search</div>
+        <div data-testid="page-search">
+          <form action="">
+            <input
+              type="text"
+              name="artistInput"
+              id="artistInput"
+              data-testid="search-artist-input"
+              onChange={ this.onInputChange }
+              value={ artistInput }
+            />
+            <button
+              data-testid="search-artist-button"
+              disabled={ buttonDisabled }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
       </>
     );
   }
