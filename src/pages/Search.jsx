61bbf561134a917be0157ddc543e46e11db9df3e
amanda-vlopes <amandavitor@hotmail.com>
2023-04-13 22:18:21 -0300

    Requisito 6 em andamento

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
 
 export default class Search extends Component {
   state = {
     artistInput: '',
     buttonDisabled: true,
+    loading: false,
+    albuns: [],
+    resultado: false,
   };
 
   onInputChange = ({ target }) => {
@@ -16,29 +21,48 @@ export default class Search extends Component {
     });
   };
 
+  handleSearchArtist = async () => {
+    this.setState({ loading: true });
+    const { artistInput } = this.state;
+    const albuns = await searchAlbumsAPI(artistInput);
+    this.setState({
+      loading: false,
+      albuns,
+      resultado: true,
+    });
+  };
+
   render() {
-    const { artistInput, buttonDisabled } = this.state;
+    const { artistInput, buttonDisabled, loading, resultado } = this.state;
+    const resultArtist = <p>{`Resultado de álbuns de:${artistInput}`}</p>;
     return (
       <>
         <Header />
-        <div data-testid="page-search">
-          <form action="">
-            <input
-              type="text"
-              name="artistInput"
-              id="artistInput"
-              data-testid="search-artist-input"
-              onChange={ this.onInputChange }
-              value={ artistInput }
-            />
-            <button
-              data-testid="search-artist-button"
-              disabled={ buttonDisabled }
-            >
-              Pesquisar
-            </button>
-          </form>
-        </div>
+        {loading
+          ? (<Carregando />)
+          : (
+            <div data-testid="page-search">
+              <form action="">
+                <input
+                  type="text"
+                  name="artistInput"
+                  id="artistInput"
+                  data-testid="search-artist-input"
+                  onChange={ this.onInputChange }
+                  value={ artistInput }
+                />
+                <button
+                  data-testid="search-artist-button"
+                  disabled={ buttonDisabled }
+                  onClick={ this.handleSearchArtist }
+                  type="button"
+                >
+                  Pesquisar
+                </button>
+              </form>
+              {resultado && resultArtist}
+            </div>
+          )}
       </>
     );
   }
